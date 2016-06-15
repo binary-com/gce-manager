1dc92e0095b74061b9b2cc5899e5b469a2107230
ssteo <teo@binary.com>
2016-06-15 11:06:55 +0800

    Work in progress

--- lib/zone.py
+++ lib/zone.py
@@ -12,7 +12,7 @@ class Zone:
         return self.pe_uptime_hour + self.npe_uptime_hour
 
     def get_termination_rate(self):
-        return (float(self.total_termination_count) / self.total_uptime_hour) if self.total_uptime_hour > 0 else 0.0
+        return (float(self.total_termination_count) / self.get_total_uptime_hour()) if self.get_total_uptime_hour() > 0 else 0.0
 
     def __repr__(self):
         return pformat(vars(self), indent=PRETTY_PRINT_INDENT, width=PRETTY_PRINT_WIDTH)
